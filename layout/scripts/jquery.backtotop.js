e5c0bd27144c563f7d2bb7b9af493dc2cc0bae0e
yannoliv <yannoliv@student.arteveldehs.be>
2017-05-24 17:36:23 +0200

    WIP

---
+++ layout/scripts/jquery.backtotop.js
@@ -0,0 +1,26 @@
+/*
+Template Name: Multiswift
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+File: Back to Top JS
+*/
+
+jQuery("#backtotop").click(function () {
+    jQuery("body,html").animate({
+        scrollTop: 0
+    }, 600);
+});
+
+jQuery("#scrolltoportfolio").click(function () {
+    jQuery("body,html").animate({
+        scrollTop: 1253
+    }, 600);
+});
+
+jQuery("#scrolltooverons").click(function () {
+    jQuery("body,html").animate({
+        scrollTop: 1853
+    }, 600);
+});
